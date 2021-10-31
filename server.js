320062ed58972852dcbffa99734d22092fa1a00f
LoopySquare <tjobin1@gmail.com>
2021-10-30 19:20:59 -0600

    added routes to display db data, line chart not displaying

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useFindAndModify: false
 });
 
-// routes
-app.use(require("./routes/api"));
+app.use(require("./routes/apiRoutes"));
+app.use(require("./routes/htmlRoutes"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);

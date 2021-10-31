dc0a08b1328d69b9bb977e739885680fbb61b88f
LoopySquare <tjobin1@gmail.com>
2021-10-30 23:52:39 -0600

    changed some formatting

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,25 +2,25 @@ const router = require("express").Router();
 const Workout = require("../models/workout");
 
 router.get("/api/workouts", (req, res) => {
-    Workout.find({})
-      .sort({ date: -1 })
-      .then(dbWorkout => {
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.status(400).json(err);
-      });
-  });
+  Workout.find({})
+    .sort({ date: -1 })
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
 
 router.get("/api/workouts/range", (req, res) => {
-    Workout.find({})
-      .then(dbWorkout => {
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.status(400).json(err);
-      });
-  });
+  Workout.find({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
 
 router.post("/api/workouts", ({ body }, res) => {
   Workout.create(body)
@@ -34,14 +34,7 @@ router.post("/api/workouts", ({ body }, res) => {
 
 router.put("/api/workouts/:id", (req, res) => {
   Workout.updateOne(
-      {
-          _id: req.params.id
-      },
-      {
-          $push: {
-              exercises: req.body
-          }
-      })
+      { _id: req.params.id }, { $push: { exercises: req.body }})
     .then(dbWorkout => {
       res.json(dbWorkout);
     })
